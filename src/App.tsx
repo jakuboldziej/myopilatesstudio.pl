b5314643907a602fb60e2451785921612afd0265
Jakub Ołdziejewski <j.oldziejewski@technisat.com>
2026-03-23 09:06:18 +0100

    layout shift, dvh

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import Instagram from "./assets/icons/instagram.png"
 
 function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <img className="w-3/4 sm:w-2/3 max-w-180" src="logo.svg" />
-
+    <div className="h-dvh flex flex-col items-center justify-center">
+      <img 
+        className="w-3/4 sm:w-2/3 max-w-180 h-auto" 
+        src="logo.svg" 
+        alt="Myo Pilates Studio Logo"
+        width="800" 
+        height="800"
+      />
 
       <span className="text-center text-7xl sm:text-9xl">Wkrótce otwarcie!</span>
 
       <div className="pt-6">
         <a className="transition-opacity hover:opacity-75" target="_blank" href="https://www.instagram.com/myopilatesstudio/">
-          <img className="w-14" src={Instagram} />
+          <img className="w-14 h-14" src={Instagram} />
         </a>
       </div>
     </div>
